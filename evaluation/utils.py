b0ed662066b12df38370789356c51aea7b6912d9
chrmolnovia <christian.moller@novia.fi>
2024-07-24 13:23:28 +0300

    iou computation

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -4,6 +4,7 @@ import fitz
 import json
 import pandas as pd
 from meri.utils import scale_coords
+from meri.utils.pydantic_models import TableContentModel
 import pdfplumber
 from typing import List
 import json
@@ -21,6 +22,11 @@ def store_as_pickle(values, store_path: str):
 def load_pickle(path):
     with open(path, "rb") as fp:   # Unpickling
         return pickle.load(fp)
+
+def load_pickle_as_df(path):
+    table = load_pickle(path)
+    tab_cells = [[cell['text'] for cell in row] for row in table]
+    return pd.DataFrame(tab_cells)
 
 def parse_table_csv(path):
     """ Reads table with ; as seperator
@@ -76,7 +82,8 @@ def parse_table_annotations(path_to_annotations_dir, path_to_pdf_dir, ann_json_n
         bboxes.append(target_bbox)
 
         # parse table content as 2d array
-        table_list = parse_table_csv(os.path.join(path_to_annotations_dir, ann["table_content_file"]))
+        #table_list = parse_table_csv(os.path.join(path_to_annotations_dir, ann["table_content_file"]))
+        table_list = TableContentModel.from_csv(os.path.join(path_to_annotations_dir, ann["table_content_file"]))
         table_contents.append(table_list)
 
         # reference to doc and table
@@ -88,3 +95,45 @@ def parse_table_annotations(path_to_annotations_dir, path_to_pdf_dir, ann_json_n
         references.append(ref)
 
     return fitz_pages, plumber_pages, bboxes, table_contents, references
+
+def bbox_union_area(boxA, boxB, intersection_area):
+    """
+    Calculates the area of union between two bounding boxes.
+    
+    Parameters:
+        boxA (list): The first bounding box in [x1, y1, x2, y2] format.
+        boxB (list): The second bounding box in [x1, y1, x2, y2] format.
+        intersection_area (float): The area of intersection between the two bounding boxes.
+        
+    Returns:
+        float: The area of union between the two bounding boxes.
+    """
+    areaA = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
+    areaB = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
+    return areaA + areaB - intersection_area
+
+def bbox_intersection_area(boxA, boxB):
+    """
+    Calculates the area of intersection between two bounding boxes.
+    
+    Parameters:
+        boxA (list): The first bounding box in [x1, y1, x2, y2] format.
+        boxB (list): The second bounding box in [x1, y1, x2, y2] format.
+        
+    Returns:
+        float: The area of intersection between the two bounding boxes.   
+    """
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    if xA < xB and yA < yB:
+        return (xB - xA) * (yB - yA)
+    else:
+        return 0
+        
+def iou(bbox_a, bbox_b):
+    intersection_area = bbox_intersection_area(bbox_a, bbox_b)
+    union_area = bbox_union_area(bbox_a, bbox_b, intersection_area)
+
+    return intersection_area / union_area if union_area>0 else 0

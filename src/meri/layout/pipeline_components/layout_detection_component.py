915de469d6f19bc45d97175d139e288710e9eb9b
chrmolnovia <christian.moller@novia.fi>
2024-09-19 15:44:41 +0300

    added yolo based layout detection component

--- src/meri/layout/pipeline_components/layout_detection_component.py
+++ src/meri/layout/pipeline_components/layout_detection_component.py
@@ -1,7 +1,11 @@
 import deepdoctection as dd
 from typing import List
 from .utils import ProcessingService
-
+from ultralytics import YOLO
+from ..settings import CustomLayoutTypes
+from deepdoctection import ModelDownloadManager, ModelCatalog
+import os
+from huggingface_hub import hf_hub_download
 
 class LayoutDetectorComponent(dd.ImageLayoutService, ProcessingService):
     ''' Execute ML-based layout analysis. Provide method to specify applied detection approach.
@@ -37,7 +41,10 @@ class LayoutDetectorComponent(dd.ImageLayoutService, ProcessingService):
             categories = dd.ModelCatalog.get_profile(cfg.WEIGHTS.DOCTR_WORD.PT).categories
             layout_detector = dd.DoctrTextlineDetector("db_resnet50", path_weights_tl, categories, cfg.DEVICE)
             kwargs = {'to_image': True, 'crop_image': True}
-
+        elif method == 'yolov10doclay':
+            
+            layout_detector = YoloDetector(cfg)
+            kwargs = {}
         else:
             raise NotImplementedError
 
@@ -52,3 +59,67 @@ class LayoutDetectorComponent(dd.ImageLayoutService, ProcessingService):
 
         if self.cover_prev_anns:
             self.uncover_annotations(dp)
+
+
+class YoloDetector(dd.ObjectDetector):
+
+    def __init__(self, cfg):
+        super().__init__()
+        self.categories = {obj.name: obj.value for obj in CustomLayoutTypes}
+        mode = "LAYOUT"
+        weights = (
+            getattr(cfg.TF, mode).WEIGHTS
+            if cfg.LIB == "TF"
+            else (getattr(cfg.PT, mode).WEIGHTS)
+        )
+
+        absolute_path_weights = ModelCatalog.get_full_path_weights(weights)
+        profile = ModelCatalog.get_profile(weights)
+        if not os.path.exists(absolute_path_weights):
+            print("loading weights from hf")
+            weights = hf_hub_download(repo_id=profile.hf_repo_id, filename=profile.hf_model_name,
+                                    local_dir=os.path.abspath(os.path.join(absolute_path_weights, os.pardir)))
+
+        self.name = "YoloDetector"
+        self.model_id = self.get_model_id()
+
+        self.model = YOLO(absolute_path_weights)
+        self.device = cfg.DEVICE
+
+        self.filter_categories = getattr(getattr(cfg.PT, "LAYOUT"), "FILTER")
+
+        self.model.to(self.device)
+
+        converted_class_names = {key: value.replace('-', '_').lower() for key, value in self.model.names.items()}
+        self.dd_aligned_class_names = {key: ('figure' if value == 'picture' else value) for key, value in converted_class_names.items()}
+
+
+
+    def predict(self, image):
+        result = self.model.predict(image)[0]
+        detections = []
+        for box, scores, pred_cls in zip(result.boxes.xyxy, result.boxes.conf, result.boxes.cls):  # xyxy format7
+            class_name = self.dd_aligned_class_names[pred_cls.item()]
+            if class_name not in self.filter_categories:
+                detections.append(dd.DetectionResult(
+                    box=box.tolist(),
+                    score=scores.item(),
+                    class_id=pred_cls.item(),
+                    class_name=class_name
+                ))
+               
+        return detections
+    
+    def possible_categories(self):
+        return list(CustomLayoutTypes)
+        #return list(self.dd_aligned_class_names.values())
+
+    @classmethod
+    def get_requirements(cls) -> List[dd.utils.detection_types.Requirement]:
+        return [dd.utils.file_utils.get_pytorch_requirement()]
+
+    def clone(self) -> 'YoloDetector':
+        return self.__class__()
+
+    def get_model_id(self) -> str:
+        return dd.utils.identifier.get_uuid_from_str(self.name)[:8]
--- src/meri/layout/settings.py
+++ src/meri/layout/settings.py
@@ -34,3 +34,27 @@ IMAGE_ANNOTATION_TO_LAYOUTS.update({CustomLayoutTypes.list_item: Layout})
 
 update_all_types_dict()
 print("CustomLayoutTypes registered:", ("CustomLayoutTypes" in dd.object_types_registry))
+
+
+dd.ModelCatalog.register("layout/docalynet/yolov10x_best.pt", dd.ModelProfile(
+    name="layout/docalynet/yolov10x_best.pt",
+    description="Yolov10 fine-tuned on doclaynet dataset. https://huggingface.co/omoured/YOLOv10-Document-Layout-Analysis",
+    tp_model=False,
+    hf_repo_id="omoured/YOLOv10-Document-Layout-Analysis",
+    hf_model_name="yolov10x_best.pt",
+   # hf_config_file="README.md",
+    size = [],
+    categories={
+        0: CustomLayoutTypes.caption,
+        1: CustomLayoutTypes.footnote,
+        2: CustomLayoutTypes.formula,
+        3: CustomLayoutTypes.list_item,
+        4: CustomLayoutTypes.page_footer,
+        5: CustomLayoutTypes.page_header,
+        6: CustomLayoutTypes.figure,
+        7: CustomLayoutTypes.section_header,
+        8: CustomLayoutTypes.table,
+        9: CustomLayoutTypes.text,
+        10: CustomLayoutTypes.title
+    }
+))

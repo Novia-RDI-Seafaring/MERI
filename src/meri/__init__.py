7b5fe6624b85fd31516f7ab85731bc58449701e6
lamboyjat <lamboyjat@gmail.com>
2024-11-12 13:38:54 +0000

    Test that the changes are working correctly

--- src/meri/__init__.py
+++ src/meri/__init__.py
@@ -1,4 +1,7 @@
-from .meri import MERI
-from .configs import MERI_CONFIGS_PATH
+from .core.meri import MERI
+from .config.config_manager import ConfigManager
 
-__all__ = ["MERI", "MERI_CONFIGS_PATH"]
+config_manager = ConfigManager()
+MERI_CONFIGS_PATH = config_manager.get_layout_config_dir()
+
+__all__ = ["MERI", "MERI_CONFIGS_PATH", "ConfigManager"]
